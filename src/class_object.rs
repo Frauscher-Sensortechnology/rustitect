4caf8581f8cff1ac8b7c5daa7e794ebb7d2f7319
Kay Fichtelmann <kay.fichtelmann@frauscher.com>
2023-07-11 10:54:11 +0200

    Introduce a struct representing documentation
    class_object.rs contains a Class and Method struct to summarize all the data into it for further processing

---
+++ src/class_object.rs
@@ -0,0 +1,19 @@
+/// Represents a class in the code, including its associated methods and documentation.
+pub struct Class {
+    /// The PlantUML diagram for the class.
+    pub plantuml: String,
+    /// The name of the class.
+    pub name: String,
+    /// The documentation for the class.
+    pub documentation: String,
+    /// The methods associated with the class.
+    pub methods: Vec<Method>,
+}
+
+/// Represents a method within a class, including its name and documentation.
+pub struct Method {
+    /// The name of the method.
+    pub name: String,
+    /// The documentation for the method.
+    pub documentation: String,
+}
