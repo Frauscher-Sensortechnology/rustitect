1e673d0383a5b06b6504ecd85bdb426b08808a37
Kay Fichtelmann <kay.fichtelmann@frauscher.com>
2023-08-24 10:00:24 +0200

    Refactor rust_doc_parser.rs
    Improve the code and
    adapt the documentation.

--- src/parser/rust_doc_parser.rs
+++ src/parser/rust_doc_parser.rs
@@ -1,4 +1,6 @@
-use syn::{Fields, Item};
+//! A module for parsing Rust code documentation and generating Markdown documentation.
+
+use syn::{Fields, Item, Meta};
 
 use crate::model::class_object::{Class, Method};
 
@@ -9,11 +11,30 @@ pub struct RustDocParser {
 }
 
 impl RustDocParser {
-    /// Parses the code documentation and returns it as a Markdown-formatted string.
+    /// Parses the given Rust code documentation and returns it in Markdown format.
+    ///
+    /// This function primarily focuses on extracting documentation of structs
+    /// and their named fields.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use crate_name::RustDocParser;
+    /// let rust_code = r#"
+    ///     /// A sample struct.
+    ///     struct Sample {
+    ///         /// A sample field.
+    ///         field: String,
+    ///     }
+    /// "#;
+    /// let parser = RustDocParser { raw_rust_code: rust_code.to_string() };
+    /// let markdown = parser.parse_code_doc_to_markdown_string();
+    /// assert!(markdown.contains("A sample struct."));
+    /// ```
     ///
     /// # Returns
     ///
-    /// The code documentation formatted as a Markdown string.
+    /// A string containing the code documentation formatted as Markdown.
     pub fn parse_code_doc_to_markdown_string(&self) -> String {
         let mut markdown = String::new();
 
@@ -23,11 +44,14 @@ impl RustDocParser {
         markdown
     }
 
-    /// Parses the code documentation and returns the Class struct containing the parsed information.
+    /// Parses the Rust code documentation and returns a representation in the form of a `Class` object.
+    ///
+    /// This function will extract the name and documentation of structs, and for each named field
+    /// inside a struct, it will treat it as a "method" with its respective documentation.
     ///
     /// # Returns
     ///
-    /// The Class struct representing the parsed code documentation.
+    /// A `Class` instance representing the parsed Rust documentation.
     pub fn parse_code_doc(&self) -> Class {
         let parsed_file = syn::parse_file(&self.raw_rust_code).unwrap();
 
@@ -35,59 +59,52 @@ impl RustDocParser {
         let mut struct_documentation = String::new();
         let mut methods = Vec::new();
         for item in parsed_file.items {
-            match item {
-                Item::Struct(item_struct) => {
-                    struct_name.push_str(&format!("{}", item_struct.ident));
-                    for attribute in item_struct.attrs {
-                        let meta = attribute.parse_meta().unwrap();
+            if let Item::Struct(item_struct) = item {
+                struct_name.push_str(&format!("{}", item_struct.ident));
+                for attribute in item_struct.attrs {
+                    let meta = attribute.parse_meta().unwrap();
+                    add_name_value_to_documentation(&mut struct_documentation, meta);
+                }
+                struct_documentation.push('\n');
 
-                        if let syn::Meta::NameValue(name_value) = meta {
-                            if name_value.path.is_ident("doc") {
-                                if let syn::Lit::Str(lit_str) = name_value.lit {
-                                    struct_documentation.push_str(&lit_str.value().trim());
-                                    struct_documentation.push('\n');
-                                }
-                            }
+                // Collect information about methods and their documentation
+                if let Fields::Named(fields) = &item_struct.fields {
+                    for field in &fields.named {
+                        let method_name = field.ident.as_ref().unwrap().to_string();
+                        let mut method_documentation = String::new();
+
+                        for attribute in &field.attrs {
+                            let meta = attribute.parse_meta().unwrap();
+                            add_name_value_to_documentation(&mut method_documentation, meta);
                         }
-                    }
-                    struct_documentation.push('\n');
 
-                    // Collect information about methods and their documentation
-                    if let Fields::Named(fields) = &item_struct.fields {
-                        for field in &fields.named {
-                            let method_name = field.ident.as_ref().unwrap().to_string();
-                            let mut method_documentation = String::new();
+                        let method = Method {
+                            name: method_name,
+                            documentation: method_documentation,
+                        };
 
-                            for attribute in &field.attrs {
-                                let meta = attribute.parse_meta().unwrap();
-                                if let syn::Meta::NameValue(name_value) = meta {
-                                    if name_value.path.is_ident("doc") {
-                                        if let syn::Lit::Str(lit_str) = name_value.lit {
-                                            method_documentation.push_str(&lit_str.value().trim());
-                                            method_documentation.push('\n');
-                                        }
-                                    }
-                                }
-                            }
-
-                            let method = Method {
-                                name: method_name,
-                                documentation: method_documentation,
-                            };
-
-                            methods.push(method);
-                        }
+                        methods.push(method);
                     }
                 }
-                _ => {}
             }
         }
-        return Class {
+        Class {
             plantuml: String::new(),
             name: struct_name,
             documentation: struct_documentation,
-            methods: methods,
-        };
+            methods,
+        }
+    }
+}
+
+fn add_name_value_to_documentation(documentation: &mut String, meta: Meta) {
+    if let Meta::NameValue(name_value) = meta {
+        if name_value.path.is_ident("doc") {
+            if let syn::Lit::Str(lit_str) = name_value.lit {
+                documentation.push_str(lit_str.value().trim());
+                documentation.push('\n');
+            }
+        }
     }
 }
 
